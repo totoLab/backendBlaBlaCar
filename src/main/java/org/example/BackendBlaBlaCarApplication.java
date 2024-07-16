d3f381d6befb3e5abfcee5e375473067babc8ea5
toto <antoniolabate19@gmail.com>
2024-07-16 17:17:21 +0200

    unnecessary

--- src/main/java/org/example/BackendBlaBlaCarApplication.java
+++ src/main/java/org/example/BackendBlaBlaCarApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({"org.example.controllers"})
 public class BackendBlaBlaCarApplication {
 
     public static void main(String[] args) {
