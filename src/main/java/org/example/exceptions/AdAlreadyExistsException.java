5eaf37ef9e8a93404fbb68462f0d21f0d1873af5
toto <antoniolabate19@gmail.com>
2024-07-18 14:05:22 +0200

    expanded and refactored service

---
+++ src/main/java/org/example/exceptions/AdAlreadyExistsException.java
@@ -0,0 +1,5 @@
+package org.example.exceptions;
+
+public class AdAlreadyExistsException extends Exception {
+    public AdAlreadyExistsException(String message) { super(message); }
+}
--- src/main/java/org/example/services/UserServices.java
+++ src/main/java/org/example/services/UserServices.java
@@ -25,66 +25,92 @@ public class UserServices {
     @Transactional(propagation = Propagation.REQUIRED, rollbackFor = {Exception.class, AdNotFoundException.class, BookingAlreadyExistsException.class, UserNotFoundException.class})
     @Lock(LockModeType.OPTIMISTIC)
     public Booking bookARide(User user, Ad ad) throws NoSeatsLeftException, AdNotFoundException, UserNotFoundException {
-        if (user != null && userRepository.existsByEmailOrUsername(user.getEmail(), user.getUsername())) {
-            if (ad != null && adRepository.existsByDepartureCityAndArrivalCityAndDate(ad.getDepartureCity(), ad.getArrivalCity(), ad.getDate())) { // TODO add two back seats handling
-                if (ad.getBooked_seats() <= 0) throw new NoSeatsLeftException("Non sono rimasti posti per questo annuncio");
-                ad.setBooked_seats(ad.getBooked_seats() - 1);
-
-                Booking booking = new Booking();
-                booking.setBooker(user);
-                userRepository.save(user);
-                booking.setAd(ad);
-                return bookingRepository.save(booking);
-            } else {
-                throw new AdNotFoundException("Annuncio " + ad + " non trovato");
-            }
-        } else {
+        if (user == null || !userRepository.existsByEmailOrUsername(user.getEmail(), user.getUsername())) {
             throw new UserNotFoundException("Utente " + user + " non trovato.");
         }
+
+        if (ad == null || !adRepository.existsByDepartureCityAndArrivalCityAndDate(ad.getDepartureCity(), ad.getArrivalCity(), ad.getDate())) {
+            throw new AdNotFoundException("Annuncio " + ad + " non trovato");
+        }
+
+        if (ad.getBookedSeats() <= 0) {
+            throw new NoSeatsLeftException("Non sono rimasti posti per questo annuncio");
+        }
+
+        ad.setBookedSeats(ad.getBookedSeats() + 1);
+        adRepository.save(ad);
+
+        Booking booking = new Booking();
+        booking.setBooker(user);
+        booking.setAd(ad);
+        return bookingRepository.save(booking);
     }
 
-    @Transactional(propagation = Propagation.REQUIRED, rollbackFor = {Exception.class, AdNotFoundException.class, BookingAlreadyExistsException.class, UserNotFoundException.class})
+    @Transactional(propagation = Propagation.REQUIRED, rollbackFor = {Exception.class, AdNotFoundException.class, UserNotFoundException.class, BookingNotFoundException.class})
     @Lock(LockModeType.OPTIMISTIC)
     public void removeBooking(User user, Ad ad) throws AdNotFoundException, UserNotFoundException, BookingNotFoundException {
-        if (user != null) {
-            if (ad != null) {
-                Booking booking = bookingRepository.findByBookerAndAd(user, ad);
-                bookingRepository.delete(booking);
-                ad.setBooked_seats(ad.getBooked_seats() + 1);
-            } else {
-                throw new AdNotFoundException("Annuncio " + ad + " non trovato");
-            }
-        } else {
+        if (user == null || !userRepository.existsByEmailOrUsername(user.getEmail(), user.getUsername())) {
             throw new UserNotFoundException("Utente " + user + " non trovato.");
         }
+
+        if (ad == null) {
+            throw new AdNotFoundException("Annuncio " + ad + " non trovato");
+        }
+
+        Booking booking = bookingRepository.findByBookerAndAd(user, ad);
+        if (booking == null) {
+            throw new BookingNotFoundException("Prenotazione non trovata.");
+        }
+
+        bookingRepository.delete(booking);
+        ad.setBookedSeats(ad.getBookedSeats() - 1);
+        adRepository.save(ad);
     }
 
     @Transactional(propagation = Propagation.REQUIRED, rollbackFor = {Exception.class, AdNotFoundException.class, UserNotFoundException.class})
-    public void addAd(User user, Ad ad) throws Exception {
-        if (user != null && userRepository.existsByEmailOrUsername(user.getEmail(), user.getUsername())) {
-            if (ad != null && ad.getMax_seats() > 0) {
-                if (!adRepository.existsById(ad.getId())) adRepository.save(ad);
-                else throw new Exception("Annuncio " + ad + " già esistente ");
-            } else {
-                throw new Exception("Invalid ad entity: " + ad);
-            }
-        } else {
+    public Long addAd(User user, Ad ad) throws Exception {
+        if (user == null || !userRepository.existsByEmailOrUsername(user.getEmail(), user.getUsername())) {
             throw new UserNotFoundException("Utente " + user + " non trovato.");
         }
+
+        if (ad == null || ad.getMaxSeats() <= 0) {
+            throw new Exception("Invalid ad entity: " + ad);
+        }
+
+        if (adRepository.existsById(ad.getId())) {
+            throw new Exception("Annuncio " + ad + " già esistente ");
+        }
+
+        User u = userRepository.findByUsername(user.getUsername());
+        ad.setPublisher(u);
+        adRepository.save(ad);
+        return ad.getId();
     }
 
     @Transactional(propagation = Propagation.REQUIRED, rollbackFor = {Exception.class, AdNotFoundException.class, UserNotFoundException.class})
     public void removeAd(User user, Ad ad) throws Exception {
-        if (user != null && userRepository.existsByEmailOrUsername(user.getEmail(), user.getUsername())) {
-            if (ad != null && ad.getPublisher().equals(user)) {
-                if (adRepository.existsById(ad.getId())) adRepository.delete(ad);
-                else throw new AdNotFoundException("Annuncio " + ad + " non trovato");
-            } else {
-                throw new Exception("Invalid ad entity: " + ad);
-            }
-        } else {
+        if (user == null || ad == null) {
+            throw new Exception("Invalid entities passed.");
+        }
+
+        if (!userRepository.existsByEmailOrUsername(user.getEmail(), user.getUsername())) {
             throw new UserNotFoundException("Utente " + user + " non trovato.");
         }
+
+        User u = userRepository.findByEmailOrUsername(user.getEmail(), user.getUsername());
+        if (!ad.getPublisher().equals(u)) {
+            throw new Exception("Utente non autorizzato alla rimozione");
+        }
+
+        if (!adRepository.existsById(ad.getId())) {
+            throw new AdNotFoundException("Annuncio " + ad + " non trovato");
+        }
+
+        adRepository.delete(ad);
+    }
+
+    public User getRandomUser() {
+        return userRepository.findByUsername("toto");
     }
 
 }
