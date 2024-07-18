98b69925d6b59c3dfb6dfe4ab548f743c060f0be
toto <antoniolabate19@gmail.com>
2024-07-18 14:06:19 +0200

    expanded repository

--- src/main/java/org/example/repositories/BookingRepo.java
+++ src/main/java/org/example/repositories/BookingRepo.java
@@ -2,7 +2,11 @@ package org.example.repositories;
 
 import org.example.entities.*;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface BookingRepo extends JpaRepository<Booking, Long>  {
     Booking findByBookerAndAd(User user, Ad ad);
+    List<Booking> findByAd(Ad ad);
+
+    boolean existsByBookerAndAdId(User user, Long id);
 }
